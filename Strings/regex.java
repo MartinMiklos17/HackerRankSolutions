00e4b4581a5af0a203b29fdb5b7350869a0b2ac0
MartinMiklos17 <35778155+MartinMiklos17@users.noreply.github.com>
2018-02-13 10:16:09 +0100

    Add files via upload

---
+++ Strings/regex.java
@@ -0,0 +1,26 @@
+/*
+Problem:  https://www.hackerrank.com/challenges/java-regex/forum
+ */
+//givenCode
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+//Solution    
+
+class MyRegex {
+
+    String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    public String pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+}
