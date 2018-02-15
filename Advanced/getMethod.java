3f2198b4c2cbc5843d0246359b8e1387226345a3
MartinMiklos17 <35778155+MartinMiklos17@users.noreply.github.com>
2018-02-15 11:04:53 +0100

    Add files via upload

---
+++ Advanced/getMethod.java
@@ -0,0 +1,21 @@
+/*
+Problem: https://www.hackerrank.com/challenges/java-reflection-attributes/problem
+*/
+//Solution  
+public class Solution {
+
+         public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method m : methods){
+            methodList.add(m.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+    }
